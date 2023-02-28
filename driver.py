193d9912ce845dd86b5fd905fa4515e37d9f8f2c
Chris Q <q.chris.d@gmail.com>
2023-02-28 15:21:48 -0500

    Driver now gets new data

--- driver.py
+++ driver.py
@@ -3,14 +3,29 @@
 """driver.py: Drives the colorshot data automation and colorimetry calculations."""
 
 ## Imports
-from data_helpers import get_filepaths, get_data
-
+import pandas as pd
+from data_helpers import get_filepaths, get_data, get_missing_rows
 
 def driver():
     """Main method of the program.
     """
+    
+    # Get data held in the master file
+    try:
+        master_data = get_data(["./all_data.xlsx"])
+    except:
+        master_data = pd.DataFrame()
+    
+    # Get data from the files
     data_filepaths = get_filepaths("./data_filepaths.confidential")
     data = get_data(data_filepaths)
+    
+    # Find new data
+    if master_data.columns == []:
+        master_data.columns = data.columns
+    new_data = get_missing_rows(data, master_data)
+    
+    #TODO Find sets of standards
 
 ## Main
 if __name__ == "__main__":
