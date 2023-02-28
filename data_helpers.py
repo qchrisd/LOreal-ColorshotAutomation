41694862a1c973bbcd0a62acf855d3c33b1b6599
Chris Q <q.chris.d@gmail.com>
2023-02-28 13:46:16 -0500

    Adds method and testing for compiling multiple excel files together

--- data_helpers.py
+++ data_helpers.py
@@ -2,10 +2,30 @@
 """data_helpers.py: Contains helpful methods for acquiring and manipulating data files and data structures."""
 
 ## Imports
-
+import pandas as pd
 
 def get_filepaths(text_file):
     with open(text_file) as file:
         lines = file.read().splitlines()
         
     return lines
+
+
+def get_data(file_paths):
+    """Gets data from a list of excel spreadsheets.
+
+    Args:
+        file_paths (list): A list of file paths to get data from.
+    """
+    
+    list_of_dfs = []
+    for path in file_paths:
+        current_file_df = pd.read_excel(path,
+                                     sheet_name="Plan")
+        list_of_dfs.append(current_file_df)
+    
+    all_data = pd.concat(list_of_dfs,
+                         ignore_index=True,
+                         axis=0)
+    
+    return all_data
--- tests/integration/test_data_helpers_integration.py
+++ tests/integration/test_data_helpers_integration.py
@@ -1,6 +1,9 @@
 
+## Imports
+import pandas as pd
 
-from data_helpers import get_filepaths
+# Testing module
+from data_helpers import get_filepaths, get_data
 
 
 def test_get_filepaths_integration():
@@ -9,6 +12,13 @@ def test_get_filepaths_integration():
     actual = get_filepaths("./tests/integration_files/test_filepaths.txt")
     assert actual == expected
     
+def test_get_data():
+    expected = pd.read_excel("./tests/integration_files/get_data expected.xlsx")
+    actual = get_data(["./tests/integration_files/Test File 1.xlsx",
+                       "./tests/integration_files/Test File 2.xlsx"])
+    pd.testing.assert_frame_equal(actual, expected)
+    
     
 if __name__ == "__main__":
     test_get_filepaths_integration()
+    test_get_data()
