373f351554127a574e67f48930dca3a16f47a5a6
Chris Q <q.chris.d@gmail.com>
2023-02-28 13:48:51 -0500

    Adds driver method to the driver file and adds the tested methods for data loading

--- driver.py
+++ driver.py
@@ -3,8 +3,15 @@
 """driver.py: Drives the colorshot data automation and colorimetry calculations."""
 
 ## Imports
+from data_helpers import get_filepaths, get_data
 
+
+def driver():
+    """Main method of the program.
+    """
+    data_filepaths = get_filepaths("./data_filepaths.confidential")
+    data = get_data(data_filepaths)
 
 ## Main
 if __name__ == "__main__":
-    pass
+    driver()
